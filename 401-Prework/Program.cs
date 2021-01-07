f14206e6284d37db6ab59836bfa0588194cbc9dc
4a50 <jpjones22@gmail.com>
2021-01-07 12:50:31 -0800

    Add project files.

---
+++ 401-Prework/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PreWorkCodeChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            fiveNumbers();
+
+        }
+        static void fiveNumbers()
+        {
+            Console.WriteLine("Enter 5 number between 1 and 10");
+            string userInput = Console.ReadLine();
+            string[] userNums = userInput.Split(' ');
+
+
+            int[] nums = new int[5];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = int.Parse(userNums[i]);
+                Console.Write(nums[i].ToString() + " ");
+            }
+
+            Console.WriteLine("Enter a number to search for:");
+            string searchString = Console.ReadLine();
+            int numberCounted = 0;
+            foreach (int q in nums)
+            {
+                if (q == int.Parse(searchString))
+                {
+                    numberCounted++;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"The number {searchString} appears {numberCounted} times");
+        }
+    }
+}
