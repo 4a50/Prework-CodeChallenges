0fa67038969ccaa195a385af160cc51e76727268
4a50 <jpjones22@gmail.com>
2021-01-07 14:05:01 -0800

    Updated Challenge 1

--- 401-Prework/Program.cs
+++ 401-Prework/Program.cs
@@ -11,30 +11,50 @@ namespace PreWorkCodeChallenges
         }
         static void fiveNumbers()
         {
-            Console.WriteLine("Enter 5 number between 1 and 10");
-            string userInput = Console.ReadLine();
-            string[] userNums = userInput.Split(' ');
-
-
+            bool enoughNums = false;
+            bool inRange = true;
+            string userInput;
+            string[] userNums;
             int[] nums = new int[5];
-            for (int i = 0; i < nums.Length; i++)
-            {
-                nums[i] = int.Parse(userNums[i]);
-                Console.Write(nums[i].ToString() + " ");
-            }
 
+
+            while (!enoughNums || !inRange)
+            {
+            Console.WriteLine("Enter 5 numbers between 1 and 10");
+                userInput = Console.ReadLine();
+                userNums = userInput.Split(' ');
+                if (userNums.Length == 5)
+                {
+                    enoughNums = true;
+                }
+                inRange = true;
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    nums[i] = int.Parse(userNums[i]);
+                    if (nums[i] < 1 || nums[i] > 10)
+                    {
+                        inRange = false;
+                    }
+                    Console.Write(nums[i].ToString() + " ");
+                }                
+            } ;
+            Console.WriteLine();
             Console.WriteLine("Enter a number to search for:");
-            string searchString = Console.ReadLine();
+            
+            int searchVal = int.Parse(Console.ReadLine());
             int numberCounted = 0;
+            
             foreach (int q in nums)
             {
-                if (q == int.Parse(searchString))
+                if (q == searchVal)
                 {
                     numberCounted++;
                 }
             }
+            int score = numberCounted * searchVal;
             Console.WriteLine();
-            Console.WriteLine($"The number {searchString} appears {numberCounted} times");
+            Console.WriteLine($"Your Score based on {searchVal} is {score}");
+
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Prework-CodeChallenges
+# Code 401 Prework-CodeChallenges
+
 Code Challenges for the ASP.NET Core Prework
+
+## Challenge 1: Array Max Result
+
+Instructions: Given an array select a number that exists, and output the computated “score”. The method you create should take in both an array of integers and the integer the user selected.
+
+Create a Console application that requests 5 numbers between 1-10 from the user. Output the array to the console and ask the user to select a number. After the selection, output the “score” of the number chosen.
+
+![] Image
