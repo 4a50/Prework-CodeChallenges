156baaabe789a982ba39532ba404aa4b61829374
4a50 <jpjones22@gmail.com>
2021-01-07 16:05:29 -0800

    Completed Sum of Rows Challenge

--- 401-Prework/Program.cs
+++ 401-Prework/Program.cs
@@ -6,12 +6,18 @@ namespace PreWorkCodeChallenges
     {
         static void Main(string[] args)
         {
-            int[] intArray = new int[] {1, 3 ,2  };            
+            int[] intArray = new int[] { 1, 3, 2 };
             //fiveNumbers();
             //leapYear(2000);
             //Console.WriteLine(perfectSequence(intArray));
-
-
+            
+            //For the sumOfRows Exercise
+            int[] sumArray = sumOfRows(4, 3);
+            string display = "Summ of the Array: [ ";
+            foreach (int i in sumArray) { display += i + " "; }
+            display += "]";
+            Console.WriteLine(display);
+            //
         }
         static void fiveNumbers()
         {
@@ -24,7 +30,7 @@ namespace PreWorkCodeChallenges
 
             while (!enoughNums || !inRange)
             {
-            Console.WriteLine("Enter 5 numbers between 1 and 10");
+                Console.WriteLine("Enter 5 numbers between 1 and 10");
                 userInput = Console.ReadLine();
                 userNums = userInput.Split(' ');
                 if (userNums.Length == 5)
@@ -41,14 +47,14 @@ namespace PreWorkCodeChallenges
                         inRange = false;
                     }
                     Console.Write(nums[i].ToString() + " ");
-                }                
-            } ;
+                }
+            };
             Console.WriteLine();
             Console.WriteLine("Enter a number to search for:");
-            
+
             int searchVal = int.Parse(Console.ReadLine());
             int numberCounted = 0;
-            
+
             foreach (int q in nums)
             {
                 if (q == searchVal)
@@ -63,7 +69,7 @@ namespace PreWorkCodeChallenges
         }
         static bool leapYear(int year)
         {
-            
+
             bool isLeapYear = false;
             //if year is /4
             if (year % 4 == 0)
@@ -75,15 +81,15 @@ namespace PreWorkCodeChallenges
                         isLeapYear = true;
                     }
                 }
-            else
+                else
                 {
                     isLeapYear = true;
                 }
 
             }
-                return isLeapYear;
+            return isLeapYear;
         }
-        
+
         static string perfectSequence(int[] intArray)
         {
             string str;
@@ -108,8 +114,38 @@ namespace PreWorkCodeChallenges
             ///Perfect Sequence: 
             /// (1) non-negative ints.  
             /// (2) product of sequence is = thier sum
-                       
+
             return str;
+        }
+
+        static int[] sumOfRows(int length, int width)
+        {
+            Random rand = new Random();                        
+            int[] sumArray = new int[length];
+            try
+            {
+                int[,] multiArray = new int[length, width];
+                int rowSum = 0;
+                int randNumber;
+                for (int i = 0; i < length; i++)
+                {                    
+                    for (int j = 0; j < width; j++)
+                    {
+                        randNumber = rand.Next(1, 20);
+                        multiArray[i, j] = randNumber;
+                        rowSum += randNumber;
+                    }
+                    sumArray[i] = rowSum;
+                    rowSum = 0;
+                }
+                Console.ReadKey();
+            }
+            catch (Exception e) {
+                Console.WriteLine("Improper Format. Ensure values are non-negative integers separated by a , [ 3,2 ]: " + e);
+            }
+
+            return sumArray;
+
         }
     }
 }
